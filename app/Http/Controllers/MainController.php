aee356f9c62f6a120a1fb894d74bcd7c14aa19fd
devrakapriyo <raka.develop@gmail.com>
2018-08-11 22:51:21 +0700

    penambahan news_detail, news_search, komentar_create

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -44,6 +44,42 @@ class MainController extends Controller
     	}
     }
 
+    public function news_detail($id, $seo)
+    {
+
+        try {
+            $data = Berita::where('id', $id)->where('seo', $seo)->first();
+            if(empty($data))
+            {
+                return Response::json("not found", 'ID Berita Tidak Tersedia', 'failed', 404);
+            }
+            return Response::json($data, 'success fetch query', 'success', 200);
+        } catch (\Exception $e) {
+            return Response::json($e->getMessage(), 'Terjadi Kesahalan', 'failed', 500);
+        }
+    }
+
+    public function news_search(Request $request)
+    {
+        try {
+            $data = Berita::where('status', 'publish')->where('judul', 'like', '%'.$request->judul.'%')->orderBy('created_at', 'DESC')->get();
+
+            $data_response = [];
+            foreach ($data as $item) {
+                $tags = Berita::where('tags', 'like', '%'.$item->tags.'%')->first();
+                if (empty($tags)) {
+                    $tags = '';
+                }
+                $data_arr = collect($item);
+                $data_arr->put('kategori', $tags);
+                $data_response[] = $data_arr;
+            }
+            return Response::json($data, 'success fetch query', 'success', 200);
+        } catch (\Exception $e) {
+            return Response::json($e->getMessage(), 'Terjadi Kesahalan', 'failed', 500);
+        }
+    }
+
     public function komentar(Request $request, $id)
     {
         try {
@@ -55,6 +91,22 @@ class MainController extends Controller
 
             $data = Komentar::where('berita_id', $id)->orderBy('created_at', 'DESC')->get();
             return Response::json($data, 'success fetch query', 'success', 200);
+        } catch (\Exception $e) {
+            return Response::json($e->getMessage(), 'Terjadi Kesahalan', 'failed', 500);
+        }
+    }
+
+    public function komentar_create(Request $request)
+    {
+        try {
+            $simpan = new Komentar;
+            $simpan->berita_id = $request->berita_id;
+            $simpan->url = $request->url;
+            $simpan->ip = $request->ip;
+            $simpan->unique = $request->unique;
+            $simpan->save();
+
+            return Response::json($simpan, 'success insert data', 'success', 200);
         } catch (\Exception $e) {
             return Response::json($e->getMessage(), 'Terjadi Kesahalan', 'failed', 500);
         }
@@ -76,10 +128,10 @@ class MainController extends Controller
         }
     }
 
-    public function profil_get($role, $status)
+    public function profil(Request $request)
     {
         try {
-            $data = User::where('role', $role)->where('status', $status)->orderBy('created_at', 'DESC')->get();
+            $data = User::where('role', $request->role)->where('status', $request->status)->orderBy('created_at', 'DESC')->get();
             return Response::json($data, 'success fetch query', 'success', 200);
         } catch (\Exception $e) {
             return Response::json($e->getMessage(), 'Terjadi Kesahalan', 'failed', 500);
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,13 @@ Route::get('/', function () {
 });
 
 Route::post('headline', 'MainController@headline');
+
 Route::post('news', 'MainController@news');
-Route::get('komentar/{id}', 'MainController@komentar');
+Route::get('news/{id}/{seo}', 'MainController@news_detail');
+Route::post('news/search', 'MainController@news_search');
 Route::get('analytics/{id}', 'MainController@analytics');
-Route::get('profil/get/{role}/{status}', 'MainController@profil_get');
+
+Route::get('komentar/{id}', 'MainController@komentar');
+Route::post('komentar', 'MainController@komentar_create');
+
+Route::post('profil', 'MainController@profil');
